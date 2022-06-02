9f449a14962ddfe2ee00761e5bbc6ffb0935ca03
Chetan <chetankhulage350@gmail.com>
2022-06-02 17:52:32 +0530

    links are now working

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -5,12 +5,5 @@ const controller = new ScrollMagic.Controller();
 
 new ScrollMagic.Scene({
     triggerElement: ".main-about",
-    offset: -300
-}).setClassToggle(".main-about", "fade-in")
-    .addTo(controller);
-
-new ScrollMagic.Scene({
-    triggerElement: ".project",
-    // offset: -300
-}).setClassToggle(".project", ".projectScroll")
-    .addTo(controller);
+    offset: -200
+}).setClassToggle(".main-about", "fade-in").addTo(controller);

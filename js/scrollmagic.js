67034b92e9235ebcfd7235abfd7bf82fddecce96
Chetan <chetankhulage350@gmail.com>
2022-06-02 14:04:06 +0530

    responsive projects tab

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -3,8 +3,14 @@ import ScrollMagic from "scrollmagic";
 
 const controller = new ScrollMagic.Controller();
 
-const Scene = new ScrollMagic.Scene({
+new ScrollMagic.Scene({
     triggerElement: ".main-about",
     offset: -300
 }).setClassToggle(".main-about", "fade-in")
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: ".project",
+    // offset: -300
+}).setClassToggle(".project", ".projectScroll")
     .addTo(controller);
--- js/tilt.js
+++ js/tilt.js
@@ -8,20 +8,24 @@ let about = document.querySelector(".main-about .main-description");
 Tilt.init(myImg, {
     reverse: true,
     speed: 700,
+    reset: false,
     max: 10,
 });
 Tilt.init(projects, {
     reverse: true,
     speed: 1500,
+    reset: false,
     max: 20,
 });
 Tilt.init(mainTitle, {
     reverse: true,
     speed: 1500,
+    reset: false,
     max: 20,
 });
 Tilt.init(about, {
     reverse: true,
     speed: 1500,
+    reset: false,
     max: 5,
 });

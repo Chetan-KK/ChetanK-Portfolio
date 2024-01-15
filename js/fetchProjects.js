a8c81dd05c52d4c39d67e67376c471a8044808f8
Chetan-KK <chetankhulage350@gmail.com>
2024-01-15 09:38:15 +0530

    api added

--- js/fetchProjects.js
+++ js/fetchProjects.js
@@ -1,5 +1,5 @@
 import Tilt from "vanilla-tilt";
-import projects from "./utils/projects";
+import fetchProjects from "./utils/GetProjects";
 
 const projectsElement = document.getElementById('projectsBox');
 
@@ -7,11 +7,8 @@ function projectsTemplate(title, imgSrc, desc, link) {
   projectsElement.innerHTML = projectsElement.innerHTML +
     `
     <div class="project">
-          <img
-            src="${imgSrc}"
-            alt="project"
-            class="project-image"
-          />
+    ${setImage(imgSrc)}
+          
           <div class="project-description">
             ${desc.slice(0, 400)}${desc.length < 400 ? "" : "..."}
             <div class="main-link cursorRespondLink">
@@ -25,20 +22,36 @@ function projectsTemplate(title, imgSrc, desc, link) {
         `;
 }
 
+const getProjects = async () => {
+  const projects = await fetchProjects();
 
-projects.map(project => {
-  projectsTemplate(
-    project.title,
-    project.imgSrc,
-    project.desc,
-    project.link
-  );
-});
-let projectsBox = document.querySelectorAll(".project");
+  projects.map(project => {
+    projectsTemplate(
+      project.title,
+      project.imgSrc,
+      project.desc,
+      project.link
+    );
+  });
+  let projectsBox = document.querySelectorAll(".project");
 
-Tilt.init(projectsBox, {
-  reverse: true,
-  speed: 1500,
-  reset: false,
-  max: 20,
-});
+  Tilt.init(projectsBox, {
+    reverse: true,
+    speed: 1500,
+    reset: false,
+    max: 20,
+  });
+};
+getProjects();
+
+function setImage(imgSrc) {
+  return `<div class="image-container">
+  <div class="placeholder flex">Loading...</div>
+  <img
+    src="${imgSrc}"
+    alt="project"
+    class="project-image"
+    onload="this.previousElementSibling.style.display='none'"
+  />
+</div>`;
+}
